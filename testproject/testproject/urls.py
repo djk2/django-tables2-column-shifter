fce5dfd46208e4fa647dbbcc473fec7a104034aa
Grzegorz Tężycki <grzegorz.tezycki@mga.com.pl>
2018-07-26 12:45:20 +0200

    Add support for Bootstrap2 and django-tables2 v2.0, close issue #7

--- testproject/testproject/urls.py
+++ testproject/testproject/urls.py
@@ -1,10 +1,11 @@
 # encoding:utf-8
 from django.conf.urls import url
 
-from .views import Bootstrap3, Bootstrap4, Index
+from .views import Bootstrap2, Bootstrap3, Bootstrap4, Index
 
 urlpatterns = [
     url(r'^$', Index.as_view(), name="index"),
+    url(r'^bootstrap2/$', Bootstrap2.as_view(), name="bootstrap2"),
     url(r'^bootstrap3/$', Bootstrap3.as_view(), name="bootstrap3"),
     url(r'^bootstrap4/$', Bootstrap4.as_view(), name="bootstrap4"),
 ]
--- testproject/testproject/views.py
+++ testproject/testproject/views.py
@@ -11,22 +11,27 @@ class Index(TemplateView):
 
 class Base(object):
 
+    container_css = ""
+    author_table_class = AuthorTable
+    book_table_class = BookTable
+
     def get_context_data(self, **kwargs):
         context = super(Base, self).get_context_data(**kwargs)
 
         # Build tabels
         author_queryset = Author.objects.all()
-        author_table1 = AuthorTable(author_queryset)
-        author_table2 = AuthorTable(author_queryset, prefix="authors2")
+        author_table1 = self.author_table_class(author_queryset)
+        author_table2 = self.author_table_class(author_queryset, prefix="authors2")
 
         book_queryset = Book.objects.all()
-        book_table = BookTable(book_queryset, prefix="books")
+        book_table = self.book_table_class(book_queryset, prefix="books")
 
         # Turn on sorting and pagination
         RequestConfig(self.request, paginate={'per_page': 2}).configure(author_table1)
         RequestConfig(self.request, paginate={'per_page': 2}).configure(author_table2)
         RequestConfig(self.request, paginate={'per_page': 2}).configure(book_table)
 
+        context['container_css'] = self.container_css
         context['author_table1'] = author_table1
         context['author_table2'] = author_table2
         context['book_table'] = book_table
@@ -34,9 +39,16 @@ class Base(object):
         return context
 
 
+class Bootstrap2(Base, TemplateView):
+    container_css = "span10 offset1"
+    template_name = "testproject/test_bootstrap2.html"
+
+
 class Bootstrap3(Base, TemplateView):
+    container_css = "col-xs-10 col-xs-offset-1"
     template_name = "testproject/test_bootstrap3.html"
 
 
 class Bootstrap4(Base, TemplateView):
+    container_css = "col-xs-10 col-xs-offset-1"
     template_name = "testproject/test_bootstrap4.html"
