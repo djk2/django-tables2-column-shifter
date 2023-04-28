a8ca391e5686691870d93c1c960fe15f606f7c9b
Marco Roose <marco.roose@mpinat.mpg.de>
2023-04-28 11:02:39 +0200

    Solved linting issue.

--- django_tables2_column_shifter/tests/urls.py
+++ django_tables2_column_shifter/tests/urls.py
@@ -14,7 +14,15 @@ urlpatterns = [
     url(r'^bootstrap2/$', Bootstrap2.as_view(), name="bootstrap2"),
     url(r'^bootstrap3/$', Bootstrap3.as_view(), name="bootstrap3"),
     url(r'^bootstrap4/$', Bootstrap4.as_view(), name="bootstrap4"),
-    url(r'^bootstrap4responsive/$', Bootstrap4Responsive.as_view(), name="bootstrap4responsive"),
+    url(
+        r'^bootstrap4responsive/$',
+        Bootstrap4Responsive.as_view(),
+        name="bootstrap4responsive",
+    ),
     url(r'^bootstrap5/$', Bootstrap5.as_view(), name="bootstrap5"),
-    url(r'^bootstrap5responsive/$', Bootstrap5Responsive.as_view(), name="bootstrap5responsive"),
+    url(
+        r'^bootstrap5responsive/$',
+        Bootstrap5Responsive.as_view(),
+        name="bootstrap5responsive",
+    ),
 ]
